c5f3edd20494eb0ae6884b9fd71d42dc2a1d9087
Thener <thenerthener@gmail.com>
2019-06-18 12:37:16 +0200

    Bottom Nav Working

--- app/src/main/java/com/mlambo/mlamboenterprise/Fragments/NotificationFragment.java
+++ app/src/main/java/com/mlambo/mlamboenterprise/Fragments/NotificationFragment.java
@@ -1,7 +1,6 @@
 package com.mlambo.mlamboenterprise.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,15 +9,9 @@ import android.view.ViewGroup;
 
 import com.mlambo.mlamboenterprise.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NotificationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
+
 public class NotificationFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
 
     public NotificationFragment() {
         // Required empty public constructor
@@ -32,42 +25,5 @@ public class NotificationFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_notification, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
--- app/src/main/java/com/mlambo/mlamboenterprise/Home.java
+++ app/src/main/java/com/mlambo/mlamboenterprise/Home.java
@@ -1,34 +1,22 @@
 package com.mlambo.mlamboenterprise;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Trace;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.mlambo.mlamboenterprise.Auth.PhoneAuth;
+import com.mlambo.mlamboenterprise.Fragments.DashboardFragment;
+import com.mlambo.mlamboenterprise.Fragments.HomeFragmant;
+import com.mlambo.mlamboenterprise.Fragments.NotificationFragment;
+
 public class Home extends AppCompatActivity {
     private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +25,48 @@ public class Home extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragmant()).commit();
+
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectFragment = null;
+
+            switch (item.getItemId())
+            {
+                case R.id.navigation_home:
+                    selectFragment = new HomeFragmant();
+                    break;
+
+                case R.id.navigation_dashboard:
+                    selectFragment = new DashboardFragment();
+                    break;
+
+                case R.id.navigation_notifications:
+                    selectFragment = new NotificationFragment();
+                    break;
+
+            }
+
+            assert selectFragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectFragment).commit();
+
+            return true;
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        startActivity(new Intent(this, PhoneAuth.class));
+    }
 }
